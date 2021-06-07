09620c90e3d2a04e60c79b50e42d5263cad9c69b
Jhonata <gutemberg.jhonata@gmail.com>
2021-06-07 10:28:49 -0300

    Change type Container to ContainerProps

--- src/components/Food/styles.ts
+++ src/components/Food/styles.ts
@@ -1,10 +1,10 @@
 import styled, { css } from 'styled-components';
 
-type Container = {
+type ContainerProps = {
   available: boolean
 }
 
-export const Container = styled.div<Container>`
+export const Container = styled.div<ContainerProps>`
   background: #f0f0f5;
   border-radius: 8px;
 
